1df89220921f1d18df57e7e434989892d996dfad
Chris McClelland <clm727@gmail.com>
2021-08-01 19:31:12 -0400

    combine student reducer, remove spread operator

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import messages from './messages';
 import auth from './auth';
+import student from './student';
 
 export default combineReducers({
   messages,
-  auth
+  auth,
+  student
 });
--- app/reducers/student.js
+++ app/reducers/student.js
@@ -5,10 +5,12 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case 'FETCH_STUDENTS':
-      return {
-        ...state,
-        items: action.payload
-      };
+      state.items = action.payload;
+      return { state };
+      // return {
+      //   ...state,
+      //   items: action.payload
+      // };
     default:
       return state;
   };
