9e3358ade3a29338e7d240729e340080e8d543db
Chris McClelland <clm727@gmail.com>
2021-08-01 19:34:04 -0400

    delete extra semicolon

--- controllers/student.js
+++ controllers/student.js
@@ -2,6 +2,6 @@ const Student = require('../models/Student');
 
 exports.getAllStudents = (req, res) => {
   return Student.find({})
-    .then(response => res.send(response));
+    .then(response => res.send(response))
     .catch(e => console.log(e));
 };
