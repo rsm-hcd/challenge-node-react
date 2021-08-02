8505dc96bbf249b14470093352f34148b113abb3
Chris McClelland <clm727@gmail.com>
2021-08-02 15:32:44 -0400

    create edit and delete components

---
+++ app/components/DeleteStudent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class DeleteStudent extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+};
+
+export default connect(mapStateToProps)(DeleteStudent);
---
+++ app/components/EditStudent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class EditStudent extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+};
+
+export default connect(mapStateToProps)(EditStudent);
