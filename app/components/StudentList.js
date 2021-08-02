6578f93dcf972695ebdcb8a7de2a45684f9373f7
Chris McClelland <clm727@gmail.com>
2021-08-01 20:05:16 -0400

    add fetchStudent action

--- app/components/StudentList.js
+++ app/components/StudentList.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStudents } from '../actions/student';
 
 class StudentList extends React.Component {
-
+  componentDidMount() {
+    this.props.fetchStudents();
+  }
 }
 
 const mapStateToProps = (state) => {
