6a58616301f53a9fbc99e6466f70213597e076da
Chris McClelland <clm727@gmail.com>
2021-08-02 15:59:35 -0400

    add actions for editing and deleting students

--- app/actions/student.js
+++ app/actions/student.js
@@ -43,3 +43,30 @@ export const showDelete = (studentId) => dispatch => {
     payload: studentId
   })
 };
+
+// update student info in the database
+export const editStudent = (studentData) => dispatch => {
+  fetch('/edit', {
+    method: 'PUT',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify(studentData)
+  })
+    .then(res => res.json())
+    .then(student =>
+      dispatch({
+        type: 'EDIT_STUDENT'
+      })
+    )
+    .catch(e => console.log(e));
+};
+
+// delete student from the database
+export const deleteStudent = (studentId) => dispatch {
+  fetch('/delete', {
+    method: 'DELETE',
+    headers: { 'Content-type': 'application/json' }
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(e => console.log(e));
+}
