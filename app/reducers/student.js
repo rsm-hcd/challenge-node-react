ed515216c5d0f9d9a31e4c015dac019dff3cc693
Chris McClelland <clm727@gmail.com>
2021-08-02 13:40:04 -0400

    fix Object.assign

--- app/reducers/student.js
+++ app/reducers/student.js
@@ -6,9 +6,9 @@ const initialState = {
 export default function student(state = initialState, action) {
   switch (action.type) {
     case 'FETCH_STUDENTS':
-      return Object.assign(state, { items: action.payload });
+      return Object.assign({}, state, { items: action.payload });
     case 'ADD_STUDENT':
-      return Object.assign(state, { item: action.payloade });
+      return Object.assign({}, state, { item: action.payloade });
     default:
       return state;
   };
