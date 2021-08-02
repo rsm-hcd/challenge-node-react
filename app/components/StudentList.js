f13946626ed485827206fad64a063d8769c0a82e
Chris McClelland <clm727@gmail.com>
2021-08-02 08:51:11 -0400

    add state and export StudentList

--- app/components/StudentList.js
+++ app/components/StudentList.js
@@ -3,11 +3,26 @@ import { connect } from 'react-redux';
 import { fetchStudents } from '../actions/student';
 
 class StudentList extends React.Component {
+  // call to the db for current list of students
   componentDidMount() {
     this.props.fetchStudents();
+  }
+
+  // add new student to the list after added to the db
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newStudent) {
+      this.props.students.push(nextProps.newStudent);
+    }
+  }
+
+  render() {
+
   }
 }
 
 const mapStateToProps = (state) => {
-
+  students: state.students.items,
+  newStudent: state.students.item
 }
+
+export default connect(mapStateToProps, { fetchStudents })(StudentList);
