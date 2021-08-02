ada7e40603dcc6f591439feca68971f6382d86d1
Chris McClelland <clm727@gmail.com>
2021-08-02 17:33:03 -0400

    add controllers for updating and deleting

--- controllers/student.js
+++ controllers/student.js
@@ -17,3 +17,27 @@ exports.addStudent = (req, res) => {
       res.status(400).send(msg: 'Student already exists');
     });
 };
+
+exports.updateStudent = (req, res) => {
+  const { _id } = req.body;
+  const update = req.body;
+  const options = { new: true };
+
+  return Student.findByIdAndUpdate(_id, update, options)
+    .then(response => res.status(200).send(response))
+    .catch(e => {
+      console.log(e);
+      res.status(400).send();
+    });
+};
+
+exports.deleteStudent = (req, res) => {
+ const { _id } = req.body;
+
+ return Student.deleteOne({ _id })
+   .then(response => res.status(200).send(response))
+   .catch(e => {
+     console.log(e);
+     res.status(400).send();
+   });
+};
