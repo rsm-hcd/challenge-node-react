d3ea62b82b35588add6167ba59affd847edd3e49
Chris McClelland <clm727@gmail.com>
2021-08-02 15:09:35 -0400

    add edit and delete buttons to student table

--- app/components/Student.js
+++ app/components/Student.js
@@ -3,12 +3,37 @@ import React from 'react';
 class Student extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      editIsVisible: false,
+      deleteisVisible: false
+    };
+
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleEdit() {
+    this.setState({ editIsVisible: !editIsVisible });
+  }
+
+  handleDelete() {
+    this.setState({ deleteisVisible: !deleteisVisible });
   }
 
   render() {
     return (
-      <div></div>
+      <tr>
+        <td>{this.props.student.name.first} {this.props.student.name.last}</td>
+        <td>{this.props.student.email}</td>
+        <td>{this.props.student.age}</td>
+        <td>{this.props.student.grade}</td>
+        <td>
+          <button onClick={this.handleEdit}>Edit</button>
+          <button onClick={this.handleDelete}>Delete</button>
+        </td>
+      </tr>
     )
   }
 }
+
+export default Student;
