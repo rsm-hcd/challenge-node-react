138bea51ee2d2c8d19b3b8ebfb5a4a2f070c360f
Chris McClelland <clm727@gmail.com>
2021-08-02 08:56:17 -0400

    add Student component

---
+++ app/components/Student.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Student extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
