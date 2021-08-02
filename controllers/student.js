045baf18f8224ae576294cc4b53dec635039dbb3
Chris McClelland <clm727@gmail.com>
2021-08-02 09:21:50 -0400

    addStudent controller

--- controllers/student.js
+++ controllers/student.js
@@ -5,3 +5,15 @@ exports.getAllStudents = (req, res) => {
     .then(response => res.send(response))
     .catch(e => console.log(e));
 };
+
+exports.addStudent = (req, res) => {
+  const student = req.body;
+  const doc = new Student(student);
+
+  return doc.save()
+    .then(response => res.send(response))
+    .catch(e => {
+      console.log(e);
+      res.status(400).send(msg: 'Student already exists');
+    });
+};
