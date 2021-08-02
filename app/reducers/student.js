981e56e07be7cabef7a3086cff270d1dd6d43902
Chris McClelland <clm727@gmail.com>
2021-08-02 11:53:49 -0400

    fix return state

--- app/reducers/student.js
+++ app/reducers/student.js
@@ -6,13 +6,9 @@ const initialState = {
 export default function student(state = initialState, action) {
   switch (action.type) {
     case 'FETCH_STUDENTS':
-      var newState = state;
-      newState.items = action.payload;
-      return { newState };
+      return Object.assign(state, { items: action.payload });
     case 'ADD_STUDENT':
-      newState = state;
-      newState.item = action.payload;
-      return { newState };
+      return Object.assign(state, { item: action.payloade });
     default:
       return state;
   };
