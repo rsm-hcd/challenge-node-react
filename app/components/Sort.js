edef4e75bb07f5490835a153ca7ce5f2ec3834bb
Chris McClelland <clm727@gmail.com>
2021-08-03 14:13:32 -0400

    add sort component

---
+++ app/components/Sort.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Sort extends React.Component {}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+export default connect(mapStateToProps, {})(Sort);
