244957c364ce5d87d8119207e4da13de3edfa1d8
Chris McClelland <clm727@gmail.com>
2021-08-03 14:10:16 -0400

    add filter component

---
+++ app/components/Filter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Filter extends React.Component {}
+
+const mapStateToProps = (state) => {
+  return (
+
+  )
+};
+
+export default Filter;
